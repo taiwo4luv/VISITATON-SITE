4665fc77bf154062e6d94953599204aa9f124066
taiwo4luv <oluwatayomi4luv@gmail.com>
2021-05-05 14:26:48 +0100

    This is my first commit

---
+++ index.py
@@ -0,0 +1,7 @@
+print("hello world")
+character_name ="tom"
+character_age ="50"
+print("There once was a man named john,")
+print("he was 35 years old,")
+print("he really liked the name john")
+print("but didn't not like being 35.")
